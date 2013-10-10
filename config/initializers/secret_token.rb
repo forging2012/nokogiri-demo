5893920c2bf243a691cff5f955886fee25ec0b28
Tommy Duek <taduek@gmail.com>
2013-10-10 10:16:51 -0400

    Initial commit and add nokogiri and addressable gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NokogiriDemoAndCurriculum::Application.config.secret_token = '1d22f1e82f2f24980d672c370960589f4c4a6c411b1694d30a5604483ec67a4db8f525156f3ea254962b669734e138d42c55b12e024c3bc3b89482b24529a531'
