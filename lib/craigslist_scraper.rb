b11e48847a64e911e030e25a69d1473dbd576559
Tommy Duek <taduek@gmail.com>
2013-10-14 14:42:46 -0400

    Fix bugs.

--- lib/craigslist_scraper.rb
+++ lib/craigslist_scraper.rb
@@ -14,18 +14,24 @@ module CraigslistScraper
     url.query = URI.encode_www_form(query_hash)
 
     page = Nokogiri::HTML(open(url.to_s))
-    scrape_page(page)
+    scrape_page(page, url)
 
     fetch_paginated_pages(url, page_num + 1) if page_num < 25
   end
 
-  def scrape_page(page)
+  def scrape_page(page, url)
     page.css(".row").each do |row|
-      apt_url = row.at_css("a")[:href]
-      craigslist_id = apt_url[/\d{6,}/].to_i
-      title = row.at_css("a").text
+      apt_url_path = row.css("a")[1][:href]
+      apt_url = url.dup
+      apt_url.path = apt_url_path
+      apt_url.query = nil
 
-      Scraping.create(url: apt_url, title: title, craigslist_id: craigslist_id)
+      craigslist_id = apt_url_path[/\d{6,}/].to_i
+      title = row.css("a")[1].text
+
+      s = Scraping.create(url:           apt_url.to_s,
+                          title:         title,
+                          craigslist_id: craigslist_id)
     end
   end
 end
